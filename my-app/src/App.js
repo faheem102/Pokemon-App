feeb090b08253cded1fe27709ca3973cea260442
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2023-11-15 16:49:17 +0100

    basic app design

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,22 @@
-
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  
+    
   return (
-    <>
-    <h1> Pokeman
-      </h1></>
+    <div className='App'>
+      <div className='titlesection'>
+      <h1>Pokeman</h1>
+      <input type= "text" 
+      onChange= {(event)=> {setPokemonName(event.target.value)}}>
+
+      </input>
+      <button >Search Pokemon</button>
+      </div>
+    </div>
   );
 }
 
